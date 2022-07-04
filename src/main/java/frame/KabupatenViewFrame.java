6c503200943f90175dc78153ce22d56a8d0df256
wikisatria <wikisatriaramadhan@gmail.com>
2022-07-04 21:56:22 +0800

    Penambahan Validasi Data Kosong dan Data Sama

--- src/main/java/frame/KabupatenViewFrame.java
+++ src/main/java/frame/KabupatenViewFrame.java
@@ -63,6 +63,14 @@ public class KabupatenViewFrame extends JFrame {
         });
 
         cariButton.addActionListener(e -> {
+            if (cariTextField.getText().equals("")){
+                JOptionPane.showMessageDialog(null,
+                        "Isi kata kunci pencarian",
+                        "Validasi kata kunci pencarian",
+                        JOptionPane.WARNING_MESSAGE);
+                cariTextField.requestFocus();
+                return;
+            }
             Connection c = Koneksi.getConnection();
             String keyword = "%" + cariTextField.getText() + "%";
             String searchSQL = "SELECT * FROM kabupaten WHERE nama like ?";
